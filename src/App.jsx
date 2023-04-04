a49532ee17c6675321c7f05dd13d430755ff1ab3
Mba96 <mbajameel90@gmail.com>
2023-04-04 20:45:26 +0000

    FunctionCounter

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ClassCounter from './ClassCounter'
+import FunctionCounter from "./FunctionCounter";
 
 function App() {
-  
-
   return (
     <div>
-      <ClassCounter />
+      <FunctionCounter />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
